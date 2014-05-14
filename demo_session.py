6ca33da17bbc48d309c38ae8e80a102c15561cc6
jlgoldb2 <jlgoldb2@asu.edu>
2014-05-14 11:31:50 -0700

    moved all .py to /lib/ folder

--- demo_session.py
+++
@@ -1,9 +0,0 @@
-
-from sqlalchemy.orm import sessionmaker
-from machine_engine import engine
-
-#use Session class constructor sessionmaker
-Session = sessionmaker(bind=engine)
-
-#create new session from Session class
-session = Session()
