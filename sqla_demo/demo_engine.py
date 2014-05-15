cab34eaf06b327be4536f635712bf4c991befe5e
Jason Goldberger <jlgoldb2@asu.edu>
2014-05-15 15:22:06 -0700

    pip freeze to include SQLAlchemy

--- sqla_demo/demo_engine.py
+++ sqla_demo/demo_engine.py
@@ -6,6 +6,6 @@
 from sqlalchemy import create_engine
 from demo_config import DBASE_URI
 
-engine = create_engine( DBASE_URI,
+engine = create_engine(DBASE_URI,
     client_encoding = 'utf8', # uses utf8
     echo=True) # prints postgres activity to stdout
